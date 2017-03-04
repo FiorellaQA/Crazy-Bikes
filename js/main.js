5a6a0f1091eadc611e4be63d06bd84ba1bdd55d6
Fiorella Quispe <fiorellaquispe@Mac-mini.local>
2017-03-04 18:36:29 -0500

    añadiendo validaciones

--- js/main.js
+++ js/main.js
@@ -2,47 +2,48 @@ function validateForm(){
 	var nombre = document.getElementById("name").value.trim();
 	var apellido = document.getElementById("lastname").value.trim();
 	var email = document.getElementById("input-email").value.trim();
-	var contraseña = document.getElementById("input-password").value.trim();
+	var password = document.getElementById("input-password").value.trim();
 	var select = document.getElementById("select").value;
 	/*Todos los campos son obligatorios, excepto los dos últimos */
-	if( ! (nombre && apellido && email && contraseña)){
+	if( ! (nombre && apellido && email && password)){
 		alert("Verifique que todos los campos estén llenos correctamente.");
 		return false;
 	}
 
-	/*Los campos nombre y apellido sólo deben permitir caracteres de la A-Z y 
-	la primera letra debe ser mayúscula*/
 	var myRe = /^([A-Z]{1}[a-zA-Z]+[\s]*)+$/;
+		if( ! (myRe.test(nombre))){
+			alert("Verificar que el nombre contenga:\n. Caracteres de la A a la Z.\n. El primer caracter (de cada nombre) en mayúscula.");
+			document.getElementById("name").focus();
+			return false;
+		} 
+		if( ! (myRe.test(apellido))){
+			alert("Verificar que el apellido contenga:\n. Caracteres de la A a la Z.\n. El primer caracter (de cada apellido) en mayúscula.");
+			document.getElementById("lastname").focus();
+			return false;
+		} 
 
-	if( ! (myRe.test(nombre))){
-		alert("Verificar que el campo contenga:\n. Caracteres de la A a la Z.\n. El primer caracter en mayúscula.");
-		document.getElementById("name").focus();
-		return false;
-	} 
-	if( ! (myRe.test(apellido))){
-		alert("Verificar que el campo contenga:\n. Caracteres de la A a la Z.\n. El primer caracter en mayúscula.");
-		document.getElementById("lastname").focus();
-		return false;
-	} 
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    	if( ! (re.test(email))){
+    		alert("Verificar que el email tenga un formato válido.\nEj: name@domain.com");
+    		document.getElementById("input-email").focus();
+    		return false;
+    	}
 
-	/*El campo password debe tener al menos 6 caracteres*/
-	if(password.length != 6){
-		alert("El password debe contener 6 caracteres.");
-		return false;
-	}
-	/*El campo password no puede ser igual a "password" ó "123456" ó "098754"*/
-	if( ! (contraseña == "password" && contraseña == "123456" && contraseña == "098754")){
-		alert("La contraseña no debe ser password/123456/098754");
-		return false;
-	}
+		if(password.length < 6){
+			alert("El password debe contener más de 6 caracteres.");
+			document.getElementById("input-password").focus();
+			return false;
+		}
 
-	if (select == "-1"){
-		alert( "Porfavor seleccione una opcion." );
-     	return false;
-	}
-	
-	/*Validar que el campo email tenga un formato válido. Ej: name@domain.com
-	
-	/*El valor seleccionado de bicis, debe ser una de las opciones presentadas*/
-	/*Si algún campo presenta error debe informarse al usuario por medio de un alert.*/
+		if( password == "password" || password == "123456" || password == "098754"){
+			alert("La contraseña no debe ser:\n. password\n. 123456\n. 098754");
+			document.getElementById("input-password").focus();
+			return false;
+		}
+		/*El valor del select debe ser contenido en el arreglo.
+		  Si no está, indexof retorna -1 */
+		if (['urbana', 'treking', 'electrica', 'estatica'].indexOf(select) < 0){
+			alert( "Porfavor seleccione una opcion." );
+     		return false;
+		}
 }
